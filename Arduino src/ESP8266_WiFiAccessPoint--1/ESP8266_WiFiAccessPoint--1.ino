2e79a3179906b30af636dbcfa708dbdfc57d4f52
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-07-29 00:06:17 +0530

    update AP - add new methods
    handleGetIntensity, handleChangePassword,
    remove bulb, chnage constant EEPROM addresses,
    add doc comment->EEPROM byte usage

--- Arduino src/ESP8266_WiFiAccessPoint--1/ESP8266_WiFiAccessPoint--1.ino
+++ Arduino src/ESP8266_WiFiAccessPoint--1/ESP8266_WiFiAccessPoint--1.ino
@@ -3,8 +3,42 @@
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 
-const char *ssid = "Default";
-const char *password = "12345678";
+/**
+ * EEPROM byte usage
+ * 
+ * 0: isFirstLoad
+ * 1: bulbCount
+ * 2:
+ * 3:
+ * 4: password[0]
+ * 5: password[1]
+ * 6: password[2]
+ * 7: password[3]
+ * 8: password[4]
+ * 9: password[5]
+ * 10: password[6]
+ * 11: password[7]
+ * 12: bulb1.id
+ * 13: bulb1.state
+ * 14: bulb1.intensityOn MSByte (Most Significant Byte)
+ * 15: bulb1.intensityOn LSByte (Least Significant Byte)
+ * 16: bulb1.intensityOff MSByte
+ * 17: bulb1.intensityOff LSByte
+ * 18: bulb2................ 6 Bytes for a bulb
+ */
+
+const char *ssid = "Main-AP";
+char *password = "12345678";
+
+const uint8_t isFirstLoadAddress = 0;
+const uint8_t bulbCountAddress = 1;
+const uint8_t isPasswordSetAddress = 2;
+const uint8_t passwordAddress = 4;
+const uint8_t startAddress = 12; //This is start address of EEPROM for store bulb details
+const int eeprom_length = 512;
+const uint8_t timeout = 2;
+const uint8_t payloadLength = 7;
+
 struct Bulb {
   uint8_t id;
   int intensityOn;
@@ -12,26 +46,35 @@ struct Bulb {
   bool state;
 };
 uint8_t bulbCount = 0;
-Bulb bulbs[] = {};
-int startAddress = 6; //This is start address of EEPROM
-const int eeprom_length = 512;
+unsigned long _time;
+int current_intensity = 0;
+int current_state = true; //true ==> up
 
 ESP8266WebServer server(80);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(115200);
   EEPROM.begin(eeprom_length);
-  //  byte len = EEPROM.length();
 
-  uint8_t isFirstLoad = EEPROM.read(0);
+  uint8_t isFirstLoad = EEPROM.read(isFirstLoadAddress);
   if (isFirstLoad == 1) { //not first loading
-    bulbCount = EEPROM.read(1); //load bulb data from ROM
+    bulbCount = EEPROM.read(bulbCountAddress); //load bulb data from ROM
+    uint8_t isPasswordSet = EEPROM.read(isPasswordSetAddress);
+    if (isPasswordSet == 1) {
+      String strPass = "";
+      for (int i = 0; i < 8; i++) {
+        strPass += (char) EEPROM.read(passwordAddress + i);
+      }
+      strPass.toCharArray(password, 9);
+    }
   } else { //if first time
-    EEPROM.write(0, 1);
+    EEPROM.write(isFirstLoadAddress, 1);
+    EEPROM.write(bulbCountAddress, 0); // set bulb count to zero
     EEPROM.commit();
-    Serial.println("firsttime");
   }
 
   WiFi.softAP(ssid, password);
@@ -42,13 +85,36 @@ void setup() {
   server.on("/changeBulbState", handleChangeBulbState);
   server.on("/getBulbState", handleGetBulbState);//wifi client
   server.on("/setIntensity", handleSetIntensity);
+  server.on("/getIntensity", handleGetIntensity);
   server.on("/getDetails", handleGetDetails);
+  server.on("/changePassword", handleChangePassword);
   server.onNotFound(handleNotFound);
   server.begin();
 }
 
 void loop() {
   server.handleClient();
+
+  // read intensity value from main arduino
+  char nextChar = Serial.read();
+  if (nextChar == 'a') {
+    String current_intensity_tmp = "";
+    int i = 0;
+    _time = millis();
+    while (i < payloadLength && (millis() - _time) < timeout) {
+      nextChar = Serial.read();
+      if (isDigit(nextChar)) {
+        current_intensity_tmp += nextChar;
+        i++;
+      } else {
+        delay(1);
+      }
+    }
+    if (current_intensity_tmp.length() == payloadLength) {
+      current_intensity = current_intensity_tmp.substring(0, 5).toInt();
+      current_state = current_intensity_tmp.substring(5).toInt();
+    }
+  }
 }
 
 
@@ -68,20 +134,25 @@ void addBulb(uint8_t id, int intensityOn, int intensityOff, bool state) {
 }
 
 void removeBulb(uint8_t bulbID) {
-  Bulb newBulbs[] = {};
-  uint8_t count = 0;
+  bool isRemoved = false;
   for (int i = 0; i < bulbCount; i++) {
-    Bulb bulb = bulbs[i];
-    if (bulb.id != bulbID) {
-      newBulbs[count] = bulb;
-      count += 1;
+    int bulbAddress = startAddress + i * 6;
+    int tempId = EEPROM.read(bulbAddress);
+    if (isRemoved) {
+      EEPROM_write_2Byte(bulbAddress + 2 - 6, EEPROM_read_2Byte(bulbAddress + 2));
+      EEPROM_write_2Byte(bulbAddress + 4 - 6, EEPROM_read_2Byte(bulbAddress + 4));
+      EEPROM.write(bulbAddress - 6, tempId);
+      EEPROM.write(bulbAddress + 1 - 6, EEPROM.read(bulbAddress + 1));
+      EEPROM.commit();
+    } else if (tempId == bulbID) {
+      isRemoved = true;
     }
   }
-  count += 1; //pointer move to next
-  for (int j = 0; j < count; j++) {
-    bulbs[j] = newBulbs[j];
+  if (isRemoved) {
+    bulbCount -= 1;
+    EEPROM.write(bulbCountAddress, bulbCount);
+    EEPROM.commit();
   }
-  bulbCount = count;
 }
 
 void changeBulbState(uint8_t bulbID, bool state) {
@@ -146,7 +217,7 @@ String getAllDetails() {
     json += (String) EEPROM.read(bulbAddress + 1);
     if (i == bulbCount - 1) {
       json += "}";
-    }else{
+    } else {
       json += "},";
     }
   }
@@ -161,7 +232,7 @@ void saveToRom(Bulb bulb) {
 
   EEPROM.write(bulbAddress, bulb.id); //Bulb block size  = 6 byte
   EEPROM.write(bulbAddress + 1, bulb.state);
-  EEPROM.write(1, bulbCount);
+  EEPROM.write(bulbCountAddress, bulbCount);
   EEPROM.commit();
 }
 
@@ -178,6 +249,19 @@ int EEPROM_read_2Byte(int address) {
   byte leastSignificant8Bit = EEPROM.read(address + 1);
   int value = (mostSignificant8Bit << 8) + leastSignificant8Bit;
   return value;
+}
+
+String changePassword(String pass, String oldPass) {
+  if (!oldPass.equals(password)) {
+    return "ERROR";
+  }
+  for (int i = 0; i < 8; i++) {
+    EEPROM.write(passwordAddress + i, pass[i]);
+  }
+  EEPROM.write(isPasswordSetAddress, 1); //password set
+  EEPROM.commit();
+  pass.toCharArray(password, 9);
+  return "OK";
 }
 
 void printEEPROM() {
@@ -199,9 +283,7 @@ void printEEPROM() {
    Handle Routes
 */
 void handleRoot() {
-  digitalWrite(LED_BUILTIN, HIGH);
   server.send(200, "application/json", "{\"res\":\"OK\"}");
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void handleAddBulb() {
@@ -295,6 +377,16 @@ void handleSetIntensity() {
   server.send ( 200, "application/json", "{\"res\":\"OK\"}" );
 }
 
+void handleGetIntensity() {
+  String res = "{\"res\":\"";
+  res += current_intensity;
+  res += ",\"state\":\"";
+  res += current_state;
+  res += "\"}";
+
+  server.send ( 200, "application/json", res );
+}
+
 void handleGetDetails() {
   String res = "{\"res\":";
   res += getAllDetails();
@@ -303,9 +395,29 @@ void handleGetDetails() {
   server.send ( 200, "application/json", res );
 }
 
+void handleChangePassword() {
+  String pass;
+  String oldPass;
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    if (server.argName ( i ) == "password") {
+      pass = server.arg ( i );
+    } else if (server.argName ( i ) == "oldPassword") {
+      oldPass = server.arg ( i );
+    }
+  }
+  String result = changePassword(pass, oldPass);
+  String res = "{\"res\":\"";
+  res += result;
+  res += "\"}";
+
+  server.send(200, "application/json", res);
+  if (result.equals("OK")) {
+    WiFi.softAP(ssid, password); //change the current password
+  }
+}
+
 
 void handleNotFound() {
-  digitalWrite(LED_BUILTIN, HIGH);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -320,5 +432,4 @@ void handleNotFound() {
   }
 
   server.send ( 404, "text/plain", message );
-  digitalWrite(LED_BUILTIN, LOW);
 }
